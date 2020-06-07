ed0d7a25a21c5edc989b4ad520813447f91bad5a
Wesley Wichman <wesley_wichman@live.com>
2020-06-07 19:41:16 +1200

    Added some relationships
    Signed-off-by: Wesley Wichman <wesley_wichman@live.com>

--- app/Board.php
+++ app/Board.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
-    //
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function lanes()
+    {
+        return $this->hasMany(Lane::class);
+    }
 }
--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function boards()
+    {
+        return $this->hasMany(Board::class);
+    }
 }
