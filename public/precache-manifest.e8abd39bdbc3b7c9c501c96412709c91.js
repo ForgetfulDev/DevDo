966e9c4af45b8a3289acd3fb889b3651596f8523
Wesley Wichman <wesley_wichman@live.com>
2020-06-14 13:59:50 +1200

    Added route to generated index file and ignore public
    Signed-off-by: Wesley Wichman <wesley_wichman@live.com>

---
+++ public/precache-manifest.e8abd39bdbc3b7c9c501c96412709c91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74331423b69b1b4d240",
+    "url": "/css/app~d0ae3f07.d86f0304.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cb009fa9ec03072b33975a052b262b63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ff4b951756283fdf7a",
+    "url": "/js/about~9d00daae.083d2a7b.js"
+  },
+  {
+    "revision": "c74331423b69b1b4d240",
+    "url": "/js/app~d0ae3f07.1e029ac9.js"
+  },
+  {
+    "revision": "144a5c31c3b077dbb993",
+    "url": "/js/chunk-vendors~d939e436.46b874e1.js"
+  },
+  {
+    "revision": "4af32f152b3e3f73b417",
+    "url": "/js/chunk-vendors~fdc6512a.e110d2c3.js"
+  },
+  {
+    "revision": "976a59dc9f21e7f251dddc4f97f2f3af",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
