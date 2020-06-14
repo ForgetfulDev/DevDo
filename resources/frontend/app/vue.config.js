779ac14277a8e2dc3fbeae478eee414d214be72f
Wesley Wichman <wesley_wichman@live.com>
2020-06-14 13:32:33 +1200

    Added storybook
    Signed-off-by: Wesley Wichman <wesley_wichman@live.com>

--- resources/frontend/app/vue.config.js
+++ resources/frontend/app/vue.config.js
@@ -1,30 +1,35 @@
 module.exports = {
     configureWebpack: {
-      optimization: {
-        splitChunks: {
-          minSize: 100000,
-          maxSize: 350000
+        optimization: {
+            splitChunks: {
+                minSize: 100000,
+                maxSize: 350000
+            }
         }
-      }
     },
-  
+    pluginOptions: {
+        storybook: {
+            allowedPlugins: ["define"]
+        }
+    },
+
     // output built static files to Laravel's public dir.
     // note the "build" script in package.json needs to be modified as well.
     outputDir: "../../../public",
     publicPath: "/",
-  
+
     // modify the location of the generated HTML file.
     indexPath: "../resources/views/home.blade.php",
     pwa: {
-      name: "DevDo",
-      themeColor: "#4DBA87",
-      msTileColor: "#000000",
-      appleMobileWebAppCapable: "yes",
-      appleMobileWebAppStatusBarStyle: "black",
-      workboxPluginMode: "GenerateSW",
-      workboxOptions: {
-        navigateFallback: '/',
-        skipWaiting: true
-      }
-    },
-  };
+        name: "DevDo",
+        themeColor: "#4DBA87",
+        msTileColor: "#000000",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "black",
+        workboxPluginMode: "GenerateSW",
+        workboxOptions: {
+            navigateFallback: "/",
+            skipWaiting: true
+        }
+    }
+};
