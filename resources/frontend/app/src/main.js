149efaab37bbf6b758b9c938a064e416fddccc93
Wesley Wichman <wesley_wichman@live.com>
2020-06-14 18:57:03 +1200

    Added foundation
    Signed-off-by: Wesley Wichman <wesley_wichman@live.com>

--- resources/frontend/app/src/main.js
+++ resources/frontend/app/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "~foundation-sites/scss/foundation.scss";
 
 Vue.config.productionTip = false;
 
