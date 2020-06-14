f395cbfc84ce4cd647955d82a3d710b26f0960fd
Wesley Wichman <wesley_wichman@live.com>
2020-06-14 12:43:45 +1200

    Added build config
    Signed-off-by: Wesley Wichman <wesley_wichman@live.com>

---
+++ resources/frontend/app/vue.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    configureWebpack: {
+      optimization: {
+        splitChunks: {
+          minSize: 100000,
+          maxSize: 350000
+        }
+      }
+    },
+  
+    // output built static files to Laravel's public dir.
+    // note the "build" script in package.json needs to be modified as well.
+    outputDir: "../../../public",
+    publicPath: "/",
+  
+    // modify the location of the generated HTML file.
+    indexPath: "../resources/views/home.blade.php",
+    pwa: {
+      name: "DevDo",
+      themeColor: "#4DBA87",
+      msTileColor: "#000000",
+      appleMobileWebAppCapable: "yes",
+      appleMobileWebAppStatusBarStyle: "black",
+      workboxPluginMode: "GenerateSW",
+      workboxOptions: {
+        navigateFallback: '/',
+        skipWaiting: true
+      }
+    },
+  };
