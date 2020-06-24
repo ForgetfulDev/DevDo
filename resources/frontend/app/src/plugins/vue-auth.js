d90678b9655b362e8d9df5ab20c7d4b219149161
Wesley Wichman <wesley_wichman@live.com>
2020-06-24 21:22:47 +1200

    Configured auth redirects
    Signed-off-by: Wesley Wichman <wesley_wichman@live.com>

--- resources/frontend/app/src/plugins/vue-auth.js
+++ resources/frontend/app/src/plugins/vue-auth.js
@@ -1,11 +1,43 @@
-import Vue             from 'vue';
-import auth            from '@websanova/vue-auth';
-import authBasic       from '@websanova/vue-auth/dist/drivers/auth/basic.esm.js';
-import httpVueResource from '@websanova/vue-auth/dist/drivers/http/vue-resource.1.x.esm.js';
-import routerVueRouter from '@websanova/vue-auth/dist/drivers/router/vue-router.2.x.esm.js';
+import Vue from "vue";
+import auth from "@websanova/vue-auth";
+import authBasic from "@websanova/vue-auth/dist/drivers/auth/basic.esm.js";
+import httpVueResource from "@websanova/vue-auth/dist/drivers/http/vue-resource.1.x.esm.js";
+import routerVueRouter from "@websanova/vue-auth/dist/drivers/router/vue-router.2.x.esm.js";
 
 Vue.use(auth, {
     auth: authBasic,
     http: httpVueResource,
     router: routerVueRouter,
+    tokenStore: ["localStorage"],
+    tokenDefaultName: "DevDo",
+    registerData: {
+        url: "/api/auth/register",
+        method: "POST",
+        redirect: "/login"
+    },
+    loginData: {
+        url: "/api/auth/login",
+        method: "POST",
+        redirect: "/dashboard",
+        fetchUser: false
+    },
+    fetchData: {
+        url: "/api/auth/user",
+        method: "POST",
+        enabled: true
+    },
+    logoutData: {
+        url: "/api/auth/logout",
+        method: "POST",
+        redirect: "/"
+    },
+    refreshData: {
+        url: "/api/auth/refresh",
+        method: "POST",
+        enabled: false,
+        interval: 30
+    },
+    authRedirect: {
+        path: "/login"
+    }
 });
