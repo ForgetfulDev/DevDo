4eaee9377b588c51a6f619236dc1e16c942d1501
Wesley Wichman <wesley_wichman@live.com>
2020-07-08 21:01:43 +1200

    Added list item for projects
    Signed-off-by: Wesley Wichman <wesley_wichman@live.com>

--- resources/frontend/app/src/Layout.vue
+++ resources/frontend/app/src/Layout.vue
@@ -1,37 +1,46 @@
 <template>
-    <v-app>
-        <v-navigation-drawer v-model="drawer" app>
-            <v-list>
-                <v-list-item>
-                    <!-- todo: Add application logo -->
-                    APPLICATION LOGO
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item>
+          <!-- todo: Add application logo -->
+          APPLICATION LOGO
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon v-text="'fas fa-project-diagram'"></v-icon>
+          </v-list-item-icon>
 
-        <v-app-bar app>
-            <v-btn @click="drawer = !drawer">Login</v-btn>
-        </v-app-bar>
+          <v-list-item-content>
+            <v-list-item-title>Projects</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <!-- Sizes your content based upon application components -->
-        <v-main>
-            <!-- Provides the application the proper gutter -->
-            <v-container fluid>
-                <!-- If using vue-router -->
-                <router-view></router-view>
-            </v-container>
-        </v-main>
+    <v-app-bar app>
+      <v-btn @click="drawer = !drawer">Login</v-btn>
+    </v-app-bar>
 
-        <v-footer app>
-            <!-- -->
-        </v-footer>
-    </v-app>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        drawer: false
-    })
+  data: () => ({
+    drawer: false
+  })
 };
 </script>
