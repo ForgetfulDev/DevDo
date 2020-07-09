1ff242363c63905de4a2e5830bd2e7750428fc85
Wesley Wichman <wesley_wichman@live.com>
2020-07-09 20:39:14 +1200

    WIP
    Signed-off-by: Wesley Wichman <wesley_wichman@live.com>

--- resources/frontend/app/src/store/actions.js
+++ resources/frontend/app/src/store/actions.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 export default {
-    actions: {
+    
         createProject(context, data) {
             console.log(data);
             axios.post("/project", data).then(response => {
             });
         }
-    }
+    
 };
